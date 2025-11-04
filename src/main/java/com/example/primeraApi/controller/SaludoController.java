6e0de3a99af933a42caf5b30080b14a2a00ba0c2
Luis Adrian Amador Martínez <triaxis12q@gmail.com>
2025-11-03 20:55:20 -0600

    Api Rest Saludo

---
+++ src/main/java/com/example/primeraApi/controller/SaludoController.java
@@ -0,0 +1,20 @@
+package com.example.primeraApi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/apisaludos")
+public class SaludoController {
+@GetMapping("/hola") 
+    public String holaMundo (){
+        return "Hola Mundo";
+    }
+  @GetMapping ("/holaname/{nombre}/{edad}")  
+ public String holaMundoNombre(@PathVariable String nombre, @PathVariable int edad){
+     return "Hola Mundo " + nombre + " tu edad es: " + edad;
+ }
+         
+}
